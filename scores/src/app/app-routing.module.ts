9662a68012fac898aaf722dd58f71dffc7e4551f
MohamadSoufan <mohamad.soufan01@libertymutual.com>
2021-06-24 01:50:09 -0400

    moved code inside app folder

--- scores/src/app/app-routing.module.ts
+++ scores/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from '../core/home/home.component';
-import { MlbComponent } from '../mlb/mlb.component';
-import { MlbBoxScoreComponent } from '../mlb/mlb-box-score/mlb-box-score.component';
+import { HomeComponent } from './core/home/home.component';
+import { MlbComponent } from './mlb/mlb.component';
+import { MlbBoxScoreComponent } from './mlb/mlb-box-score/mlb-box-score.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomeComponent },
--- scores/src/app/app.module.ts
+++ scores/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from '../shared/shared.module';
-import { CoreModule } from '../core/core.module';
-import { MlbModule } from '../mlb/mlb.module';
+import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
+import { MlbModule } from './mlb/mlb.module';
 
 @NgModule({
   declarations: [AppComponent],
